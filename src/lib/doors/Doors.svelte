072323d783610426da0a4269f99fad879294677d
Jane Fan <jfan9988@gmail.com>
2022-09-17 18:42:24 +1000

    door focus animation

--- src/lib/doors/Doors.svelte
+++ src/lib/doors/Doors.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
   import { base } from '$app/paths';
+  import { fade } from 'svelte/transition';
 
   export let doorImg = `${base}/img/door-left.jpg`;
 
   let doorIsOpen = false;
+  let width = 0;
 
-  const handleClick = () => {
+  const openDoors = () => {
     doorIsOpen = !doorIsOpen;
   };
 </script>
 
-<div class="door" class:open={doorIsOpen} on:click={handleClick}>
-  <img src={doorImg} alt="door-left" />
+<div
+  bind:clientWidth={width}
+  class="door"
+  class:open={doorIsOpen}
+  on:click={openDoors}
+  style="left: {doorIsOpen ? `calc(50vw - ${width / 2}px)` : '0px'}"
+>
+  {#if doorIsOpen}
+    <div class="door-bg" transition:fade />
+  {/if}
+  <img class="left-door" src={doorImg} alt="door-left" />
   <img class="right-door" src={doorImg} alt="door-right" />
 </div>
 
@@ -20,18 +31,33 @@
     display: flex;
     position: relative;
     height: 70vh;
-    cursor: pointer;
     flex-shrink: 0;
-    transition: left 800ms ease, height 300ms ease;
+    transition: height 300ms ease;
   }
 
   .door.open {
+    position: absolute;
     height: 100%;
-    transition: left 800ms ease, height 300ms ease;
     z-index: 1;
+  }
+
+  .door-bg {
+    background-color: var(--c-black);
+    position: fixed;
+    inset: 0;
+    transition: opacity 500ms ease;
+    display: flex;
+    justify-content: center;
+    pointer-events: none;
+  }
+
+  .left-door {
+    transform: scaleX(1);
+    cursor: pointer;
   }
 
   .right-door {
     transform: scaleX(-1);
+    cursor: pointer;
   }
 </style>
