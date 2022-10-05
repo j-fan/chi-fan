df642728cc4c7f347f025f10706f63d8365eb9e8
Jane Fan <jfan9988@gmail.com>
2022-10-06 01:26:44 +1100

    update drag target image

--- src/lib/slides/variations/BaseSlide.svelte
+++ src/lib/slides/variations/BaseSlide.svelte
@@ -25,5 +25,6 @@
   .slide-content {
     width: 100%;
     height: 100%;
+    overflow: hidden;
   }
 </style>
--- src/routes/sugar-cane-water/intro/PeelingSugarCane.svelte
+++ src/routes/sugar-cane-water/intro/PeelingSugarCane.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
+  import { base } from '$app/paths';
+
   export let progress = 0;
+
+  const src = `${base}/img/sugar-cane-water/cut-cane.png`;
 </script>
 
-<p class="wrapper" style="filter: brightness({1 - progress})">sugar cane</p>
+<div class="wrapper" style="filter: brightness({1 - progress}); background-image: url({src});" />
 
 <style>
   .wrapper {
-    border: var(--border-style);
-    border-radius: var(--border-radius);
-    padding: 1rem;
-    background-color: var(--c-light-green);
-    transition: all 300ms ease;
-    width: 60px;
-    height: 400px;
+    width: 300px;
+    height: 600px;
+    background-size: contain;
+    background-position: center center;
+    background-repeat: no-repeat;
   }
 </style>
