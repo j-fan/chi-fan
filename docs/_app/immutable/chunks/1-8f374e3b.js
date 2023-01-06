1db0c0fa08b35ea13493eb925f2967817cab03b0
Jane Fan <jfan9988@gmail.com>
2023-01-06 15:56:13 +1100

    build

--- docs/_app/immutable/chunks/1-8f374e3b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-1907852f.js";export{t as component};
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,15 +1,15 @@
 const f = [
   "/chi-fan/_app/immutable/assets/fira-mono-greek-400-normal-a8be01ce.woff2",
   "/chi-fan/_app/immutable/assets/fira-mono-cyrillic-ext-400-normal-3df7909e.woff2",
-  "/chi-fan/_app/immutable/assets/fira-mono-cyrillic-400-normal-c7d433fd.woff2",
+  "/chi-fan/_app/immutable/assets/fira-mono-latin-ext-400-normal-6bfabd30.woff2",
   "/chi-fan/_app/immutable/assets/fira-mono-greek-ext-400-normal-9e2fe623.woff2",
   "/chi-fan/_app/immutable/assets/fira-mono-latin-400-normal-e43b3538.woff2",
-  "/chi-fan/_app/immutable/assets/fira-mono-latin-ext-400-normal-6bfabd30.woff2",
+  "/chi-fan/_app/immutable/assets/fira-mono-cyrillic-400-normal-c7d433fd.woff2",
   "/chi-fan/_app/immutable/assets/fira-mono-all-400-normal-1e3b098c.woff",
-  "/chi-fan/_app/immutable/start-c5d4269c.js",
-  "/chi-fan/_app/immutable/components/pages/_layout.svelte-2df0d085.js",
+  "/chi-fan/_app/immutable/start-b36f76c3.js",
+  "/chi-fan/_app/immutable/components/pages/_layout.svelte-0ccba6c6.js",
   "/chi-fan/_app/immutable/assets/_layout-20df1867.css",
-  "/chi-fan/_app/immutable/components/error.svelte-1907852f.js",
+  "/chi-fan/_app/immutable/components/error.svelte-ec8244bc.js",
   "/chi-fan/_app/immutable/components/pages/_page.svelte-b3e16916.js",
   "/chi-fan/_app/immutable/assets/_page-04228c89.css",
   "/chi-fan/_app/immutable/components/pages/about/_page.svelte-7e5f3452.js",
@@ -21,17 +21,17 @@ const f = [
   "/chi-fan/_app/immutable/assets/_page-2c0872ed.css",
   "/chi-fan/_app/immutable/modules/pages/_layout.ts-b8ee4d7c.js",
   "/chi-fan/_app/immutable/modules/pages/about/_page.ts-6f405366.js",
-  "/chi-fan/_app/immutable/chunks/singletons-bea4f6a1.js",
+  "/chi-fan/_app/immutable/chunks/singletons-47991a0e.js",
   "/chi-fan/_app/immutable/chunks/paths-d2a11cad.js",
   "/chi-fan/_app/immutable/chunks/index-077db8d7.js",
-  "/chi-fan/_app/immutable/chunks/stores-297c86a5.js",
+  "/chi-fan/_app/immutable/chunks/stores-a56ba11d.js",
   "/chi-fan/_app/immutable/chunks/_layout-1daba58d.js",
   "/chi-fan/_app/immutable/chunks/Button-d9a2d830.js",
   "/chi-fan/_app/immutable/assets/Button-4fed2903.css",
   "/chi-fan/_app/immutable/chunks/_page-a84837a2.js",
   "/chi-fan/_app/immutable/chunks/index-94a3dd3c.js",
-  "/chi-fan/_app/immutable/chunks/0-9a503cec.js",
-  "/chi-fan/_app/immutable/chunks/1-8f374e3b.js",
+  "/chi-fan/_app/immutable/chunks/0-005dd3c3.js",
+  "/chi-fan/_app/immutable/chunks/1-a0c70c2c.js",
   "/chi-fan/_app/immutable/chunks/2-e03bddfd.js",
   "/chi-fan/_app/immutable/chunks/3-d344bc25.js",
   "/chi-fan/_app/immutable/chunks/4-e05f5763.js",
@@ -62,40 +62,40 @@ const f = [
   "/chi-fan/img/sugar-cane-water/tub.jpg",
   "/chi-fan/manifest.json",
   "/chi-fan/robots.txt"
-], i = "1672980506230", c = self, p = `cache${i}`, o = f.concat(h), r = new Set(o);
-c.addEventListener("install", (a) => {
+], i = "1672980954154", s = self, p = `cache${i}`, o = f.concat(h), r = new Set(o);
+s.addEventListener("install", (a) => {
   a.waitUntil(
     caches.open(p).then((e) => e.addAll(o)).then(() => {
-      c.skipWaiting();
+      s.skipWaiting();
     })
   );
 });
-c.addEventListener("activate", (a) => {
+s.addEventListener("activate", (a) => {
   a.waitUntil(
     caches.keys().then(async (e) => {
-      for (const s of e)
-        s !== p && await caches.delete(s);
-      c.clients.claim();
+      for (const c of e)
+        c !== p && await caches.delete(c);
+      s.clients.claim();
     })
   );
 });
 async function u(a) {
   const e = await caches.open(`offline${i}`);
   try {
-    const s = await fetch(a);
-    return e.put(a, s.clone()), s;
-  } catch (s) {
+    const c = await fetch(a);
+    return e.put(a, c.clone()), c;
+  } catch (c) {
     const n = await e.match(a);
     if (n)
       return n;
-    throw s;
+    throw c;
   }
 }
-c.addEventListener("fetch", (a) => {
+s.addEventListener("fetch", (a) => {
   if (a.request.method !== "GET" || a.request.headers.has("range"))
     return;
-  const e = new URL(a.request.url), s = e.protocol.startsWith("http"), n = e.hostname === self.location.hostname && e.port !== self.location.port, t = e.host === self.location.host && r.has(e.pathname), m = a.request.cache === "only-if-cached" && !t;
-  s && !n && !m && a.respondWith(
+  const e = new URL(a.request.url), c = e.protocol.startsWith("http"), n = e.hostname === self.location.hostname && e.port !== self.location.port, t = e.host === self.location.host && r.has(e.pathname), m = a.request.cache === "only-if-cached" && !t;
+  c && !n && !m && a.respondWith(
     (async () => t && await caches.match(a.request) || u(a.request))()
   );
 });
