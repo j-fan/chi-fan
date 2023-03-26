41c05351ff99f485c78f404b3c5307e58e4b1c7f
Jane Fan <jfan9988@gmail.com>
2023-03-26 18:46:13 +1100

    lobster steps up to washing carapace

--- src/routes/live-lobster/intro/+page.svelte
+++ src/routes/live-lobster/intro/+page.svelte
@@ -91,6 +91,61 @@
       bottomPosition: 45
     },
     {
+      type: 'pick-items',
+      dialogs: [
+        {
+          characterName: 'Dad',
+          bodyText: 'Flip the carapace piece so that it is upside down with the legs facing you. ',
+          nextButton: { text: 'Continue' }
+        },
+        {
+          characterName: 'Dad',
+          bodyText:
+            'Using one of the techniques described above, slice this section in half lengthwise.',
+          nextButton: { text: 'Continue' }
+        }
+      ],
+      errorStep: {
+        characterName: 'Dad',
+        bodyText: "You haven't cut the carapace in half yet."
+      },
+      successStep: {
+        characterName: 'Dad',
+        bodyText: 'The lobster should now be dead. It is time to clean it.',
+        nextButton: { text: 'Ok' }
+      },
+      targetCount: 1,
+      totalItems: 1,
+      itemComponent: ChopLobster,
+      bgImage: `${base}/img/live-lobster/chopping-board.png`,
+      toolComponent: ChoppingKnife,
+      bottomPosition: 45
+    },
+    {
+      type: 'static',
+      heroImage: `${base}/img/live-lobster/half-lobster-washing.png`,
+      alt: 'Washing the lobster carapace',
+      dialogs: [
+        {
+          characterName: 'Dad',
+          bodyText: 'Rinse the halves of the carapace and remove the grey gristle and discard it.',
+          nextButton: { text: 'Continue' }
+        },
+        {
+          characterName: 'Dad',
+          bodyText: 'If the lobster is female, you will see a long reddish organ.',
+          nextButton: { text: 'Continue' }
+        }
+      ],
+      successStep: {
+        characterName: 'Dad',
+        bodyText:
+          'That is the reproductive organ and can be cooked and eaten, but it is an acquired taste.',
+        nextButton: { text: 'Ok' }
+      },
+      heightPercent: 60
+    },
+    {
       type: 'finished',
       heroImage: `${base}/img/live-lobster/lobster-dish.png`,
       title: '☆ Live lobster stir fry ☆',
